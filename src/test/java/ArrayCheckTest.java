5a961079eb250a02b740fbb70436bd991712ae55
crangoren <crangoren@gmail.com>
2021-06-23 16:25:31 +0300

    add homework

--- src/test/java/ArrayCheckTest.java
+++ src/test/java/ArrayCheckTest.java
@@ -1,5 +1,5 @@
-import org.junit.Assert;
-import org.junit.Before;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ArrayCheckTest {
@@ -14,9 +14,9 @@ public class ArrayCheckTest {
         int[] expected1 = {9};
         int[] arrayToTest2 = {4, 4, 4, 4, 2, 3, 2, 1, 7, 8};
         int[] expected2 = {2, 3, 2, 1, 7, 8};
-        Assert.assertArrayEquals(expected, arrayCheck.getPartAfterFour(arrayToTest));
-        Assert.assertArrayEquals(expected1, arrayCheck.getPartAfterFour(arrayToTest1));
-        Assert.assertArrayEquals(expected2, arrayCheck.getPartAfterFour(arrayToTest2));
+        Assertions.assertArrayEquals(expected, arrayCheck.getPartAfterFour(arrayToTest));
+        Assertions.assertArrayEquals(expected1, arrayCheck.getPartAfterFour(arrayToTest1));
+        Assertions.assertArrayEquals(expected2, arrayCheck.getPartAfterFour(arrayToTest2));
     }
 
     @Test
@@ -24,8 +24,8 @@ public class ArrayCheckTest {
         int[] array1 = {1, 2, 0, 4, 0, 3, 1, 5, 7, 9};
         int[] array2 = {3, 2, 3, 5, 0};
         int[] array3 = {1, 2, 9};
-        Assert.assertEquals(true, arrayCheck.checkArrayForNums(array1));
-        Assert.assertEquals(false, arrayCheck.checkArrayForNums(array2));
-        Assert.assertEquals(true, arrayCheck.checkArrayForNums(array3));
+        Assertions.assertEquals(true, arrayCheck.checkArrayForNums(array1));
+        Assertions.assertEquals(false, arrayCheck.checkArrayForNums(array2));
+        Assertions.assertEquals(true, arrayCheck.checkArrayForNums(array3));
     }
 }
